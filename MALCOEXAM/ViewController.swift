24c64b33a8c5e8544b8f3bcd898f56153ce39391
millensys <mi>
2021-02-04 10:20:05 +0200

    Initial Commit

---
+++ MALCOEXAM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MALCOEXAM
+//
+//  Created by Mostafa on 2/4/21.
+//  Copyright © 2021 admin1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
